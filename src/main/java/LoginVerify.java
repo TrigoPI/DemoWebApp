7d823dcc3c602795c0685d2f945053a3f498e18f
noName <git.demo@gmail.com>
2022-11-07 15:59:22 +0100

    first

---
+++ src/main/java/LoginVerify.java
@@ -0,0 +1,53 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.annotation.WebServlet;
+/**
+ * Servlet implementation class LoginVerify
+ */
+
+@WebServlet("/LoginVerify")
+public class LoginVerify extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginVerify() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String uname = request.getParameter("uname");
+		String pwd = request.getParameter("password");
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Username is :" + uname + "<br/>");
+		out.println("Password is :" + pwd);
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
